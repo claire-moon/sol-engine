e2c012c5e06e71be239acda43ba505e7e438cf08
cg <cg@helloworldtwo.com>
2026-08-09 21:24:53 -0500

    Update SOL player regression contract tests

--- tests/test-sol-story.sh
+++ tests/test-sol-story.sh
@@ -9,6 +9,7 @@ portal_guard="$root/sol/game/zscript/sol/portal_guard.zs"
 bootstrap="$root/sol/game/zscript/sol/bootstrap.zs"
 mapinfo="$root/sol/game/MAPINFO"
 level_travel="$root/src/g_level.cpp"
+p_map="$root/src/playsim/p_map.cpp"
 
 grep -Fx '#include "zscript/sol/story_ids.zs"' "$entry" >/dev/null
 grep -Fx '#include "zscript/sol/story_state.zs"' "$entry" >/dev/null
@@ -23,11 +24,15 @@ grep -F 'Array<int> HeardRadio;' "$state" >/dev/null
 grep -F 'int CurrentObjective;' "$state" >/dev/null
 ! grep -Eq '^[[:space:]]*transient[[:space:]]+(int|Array<int>)[[:space:]]+(ContractVersion|CurrentObjective|FiredEvents|CompletedObjectives|SeenSubtitles|HeardRadio)' "$state"
 grep -F 'class SolPlayer : DoomPlayer' "$portal_guard" >/dev/null
-grep -F 'override bool CanCrossLine(Line crossing, Vector3 next)' "$portal_guard" >/dev/null
-grep -F 'crossing.Special != 156' "$portal_guard" >/dev/null
-grep -F 'crossing.Args[2] != LinePortal.PORTT_LINKED' "$portal_guard" >/dev/null
-grep -F 'crossing.Args[0] != 9001 && crossing.Args[0] != 9002' "$portal_guard" >/dev/null
-grep -F 'moveX * forward.X + moveY * forward.Y < 0.0' "$portal_guard" >/dev/null
+grep -F 'Player.StartItem "Fist";' "$portal_guard" >/dev/null
+grep -F 'Player.StartItem "Clip", 50;' "$portal_guard" >/dev/null
+! grep -F 'Player.StartItem "Pistol";' "$portal_guard" >/dev/null
+! grep -F 'CanCrossLine' "$portal_guard" >/dev/null
+grep -F 'const DVector2 solmove = tm.pos.XY() - thing->Pos().XY();' "$p_map" >/dev/null
+grep -F 'ld->args[2] == PORTT_LINKED' "$p_map" >/dev/null
+grep -F '(ld->args[0] == 9001 || ld->args[0] == 9002)' "$p_map" >/dev/null
+grep -F '(solmove | thing->Angles.Yaw.ToVector()) < 0.0' "$p_map" >/dev/null
+grep -F 'if (sollocaltraversal)' "$p_map" >/dev/null
 grep -F 'PlayerClasses = "SolPlayer"' "$mapinfo" >/dev/null
 grep -F 'void EnsureStoryState(int playerNumber)' "$bootstrap" >/dev/null
 grep -F 'pawn.FindInventory(storyType)' "$bootstrap" >/dev/null
